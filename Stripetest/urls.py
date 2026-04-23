a7e97f928349ab05e7ecd56f81a3d2cff0d84be4
Alen <alen.makanov23@gmail.com>
2026-04-24 04:33:03 +0500

    Админ

--- Stripetest/urls.py
+++ Stripetest/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.urls import path
 from stripeApp.views import shop_page, CreateOrderView, stripe_webhook_view, item_detail
@@ -5,9 +6,18 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', shop_page, name='shop'),
+    path('/', shop_page, name='shop'),
     path('api/create-order/', CreateOrderView.as_view(), name='api_create_order'),
     path('api/webhook/', stripe_webhook_view, name='stripe_webhook'),
     path('success/', lambda request: render(request, 'success.html'), name='success'),
     path('item/<int:id>/', item_detail, name='item_detail'),
 ]
+
+User = get_user_model()
+try:
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin12345')
+    else:
+        print("ℹАдмин уже существует")
+except Exception as e:
+    print(f"⚠️ Ошибка при создании админа: {e}")
