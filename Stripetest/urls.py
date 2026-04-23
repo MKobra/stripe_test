a593e4eb64d0cf25b3649e0343c8de9ca3690228
Alen <alen.makanov23@gmail.com>
2026-04-24 04:35:05 +0500

    Админ

--- Stripetest/urls.py
+++ Stripetest/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', shop_page, name='shop'),
+    path('', shop_page, name='shop'),
     path('api/create-order/', CreateOrderView.as_view(), name='api_create_order'),
     path('api/webhook/', stripe_webhook_view, name='stripe_webhook'),
     path('success/', lambda request: render(request, 'success.html'), name='success'),
